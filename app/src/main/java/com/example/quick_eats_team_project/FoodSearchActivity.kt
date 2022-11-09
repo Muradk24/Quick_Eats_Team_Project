21b6bf097ba04b2da4d368a3324e486dfc9cd188
Murad Khan <Muradak1234@gmail.com>
2022-11-08 22:08:24 -0500

    changes in ui

--- app/src/main/java/com/example/quick_eats_team_project/FoodSearchActivity.kt
+++ app/src/main/java/com/example/quick_eats_team_project/FoodSearchActivity.kt
@@ -2,10 +2,17 @@ package com.example.quick_eats_team_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class FoodSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_search)
+
+        val imageButton=findViewById<ImageButton>(R.id.imageButton)
+        imageButton.setOnClickListener {
+            Toast.makeText(this,"Apply for all",Toast.LENGTH_SHORT).show()
+        }
     }
 }
