d85445ec246401960b1abef3e73bcad6100c9bfe
Murad Khan <Muradak1234@gmail.com>
2022-12-03 02:06:51 -0500

    complete the UI

---
+++ app/src/main/java/com/example/quick_eats_team_project/CartScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quick_eats_team_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_screen)
+    }
+}
--- app/src/main/java/com/example/quick_eats_team_project/FoodSearchActivity.kt
+++ app/src/main/java/com/example/quick_eats_team_project/FoodSearchActivity.kt
@@ -9,10 +9,14 @@ class FoodSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_search)
+        var fastFood= findViewById<ImageButton>(R.id.imageButton2)
 
         val imageButton=findViewById<ImageButton>(R.id.imageButton)
         imageButton.setOnClickListener {
             Toast.makeText(this,"Apply for all",Toast.LENGTH_SHORT).show()
         }
     }
+    val imageButton2 = findViewById<ImageButton>(R.id.imageButton2)
+
+
 }
